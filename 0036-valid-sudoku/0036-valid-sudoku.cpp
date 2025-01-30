fb1f9a380e687d9b0928764677556361f9bbb448
NathanLewis263 <144401140+NathanLewis263@users.noreply.github.com>
2025-01-30 21:45:56 +1100

    Time: 11 ms (16.57%), Space: 25.8 MB (17.31%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,7 +3,7 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_set<char> rows[9];
         unordered_set<char> cols[9];
-        unordered_set<char> box[9];
+        unordered_set<char> box[3][3];
         for (int r = 0;r<9;++r){
             for (int c = 0;c<9;++c){
                 if (board.at(r).at(c) == '.'){
@@ -11,16 +11,15 @@ public:
                 
                 }  
                 char value = board.at(r).at(c);
-                int boxIndex = (r / 3) * 3 + (c / 3);
                 if ((rows[r].find(value) != rows[r].end()) || 
                     (cols[c].find(value) != cols[c].end()) || 
-                    (box[boxIndex].find(value) != box[boxIndex].end())) {
+                    (box[r/3][c/3].find(value) != box[r/3][c/3].end())) {
                         return false;
                     }
                 
                 rows[r].insert(value);
                 cols[c].insert(value);
-                box[boxIndex].insert(value); 
+                box[r/3][c/3].insert(value); 
             }
         }
         return true;
