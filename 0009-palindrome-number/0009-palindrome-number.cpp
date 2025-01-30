bb39be667a928519ef2bf21c31bf041224f93b4b
NathanLewis263 <144401140+NathanLewis263@users.noreply.github.com>
2025-01-30 22:55:37 +1100

    Time: 2 ms (52.39%), Space: 8.5 MB (68.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0){
+            return false;
+        } else if (x < 10){
+            return true;
+        }
+
+        long z=x;
+        long y=0;
+        while (x>0){
+            long lastDigit = x % 10;
+            x /= 10;
+            y = y * 10;
+            y+=lastDigit;
+        }
+        return (z==y?true:false);
+    }
+};
