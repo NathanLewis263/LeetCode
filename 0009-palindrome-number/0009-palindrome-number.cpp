29b40f07fb38846ead46b07b6b0f1e7d079985d3
NathanLewis263 <144401140+NathanLewis263@users.noreply.github.com>
2025-01-30 23:03:41 +1100

    Time: 2 ms (52.39%), Space: 8.5 MB (92.35%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x<0 || (x%10==0 && x!=0)){
+        if (x<0){
             return false;
         } else if (x < 10){
             return true;
