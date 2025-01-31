07a929521b6ce1d7f3c6015139e56f1377da1c2a
NathanLewis263 <144401140+NathanLewis263@users.noreply.github.com>
2025-01-31 19:32:54 +1100

    Time: 0 ms (100%), Space: 10.9 MB (46.7%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -9,9 +9,7 @@ public:
         }
         word = "";
         for (auto it = splitString.end()-1;it!=splitString.begin()-1;it--){
-            for (int i = 0;i<40;i++){
-                cout << i;
-            }
+            for (int i = 0;i<40;i++){}
             word+=*it;
             if (it!=splitString.begin()){
                 word+=" ";
