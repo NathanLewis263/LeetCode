2fce8c943cdaa539a07752271636f0f177a5e6f1
NathanLewis263 <144401140+NathanLewis263@users.noreply.github.com>
2025-01-31 20:27:42 +1100

    Time: 0 ms (100%), Space: 13.7 MB (74.04%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -2,22 +2,29 @@ class Solution {
 public:
     int compress(vector<char>& chars) {
       string s = "";
+      
       for (int i = 0;i<chars.size();i++){
-        s+=chars[i];
+        s+=chars[i]; //append char to string
         char currentChar = chars[i];
         int count = 1;
+        //count how many consecutive repeating chars exist;
         while ((i+1 != chars.size() && currentChar == chars[i+1])){
             count++;
             i++;
         }
+        //append to string only if count is more than 1;
         if (count>1){
             s+=to_string(count);
         }
       }
+      
+      //modify input array;
       chars.resize(s.size());
       for (int i = 0;i<s.size();i++){
         chars[i] = s[i];
       }
-      return s.size();
+
+      //return size;
+      return chars.size();
     }
 };
