10d64cc09b708dc8b116e8c2f9726663110b9d1d
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-02 12:16:02 +1100

    Time: 1 ms (30.35%), Space: 35.6 MB (94.94%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int sum = 0;
+        int sum = accumulate(nums.begin(), nums.end(),0);
         int leftSum = 0;
-        for (int num : nums){
-            sum += num;
-        }
+        // for (int num : nums){
+        //     sum += num;
+        // }
         for (int i = 0;i<nums.size();i++){
             int rightSum = sum - leftSum - nums.at(i);
             if (leftSum == rightSum){
