936f97aa67a16774b578b1a46563f40ea4f30a3e
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-02 13:25:27 +1100

    Time: 22 ms (61.11%), Space: 36.8 MB (78.05%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cpp
@@ -1,17 +1,19 @@
 class Solution {
-private:
-    vector<int> distictinOne(vector<int>& nums1, vector<int>& nums2){
-        unordered_set<int> set1 (nums2.begin(),nums2.end());
-        unordered_set<int> set2;
-        for (auto num : nums1){
-            if (!set1.count(num)){
-                set2.insert(num);
-            }
-        }
-        return vector<int> (set2.begin(),set2.end());
-    }
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        return {distictinOne(nums1,nums2),distictinOne(nums2,nums1)};
+        unordered_set<int> set1 (nums1.begin(),nums1.end());
+        unordered_set<int> set2 (nums2.begin(),nums2.end());
+        vector<vector<int>> answer(2);
+        for (auto num : set1){
+            if (!set2.count(num)){
+                answer[0].push_back(num);
+            }
+        }
+        for (auto num : set2){
+            if (!set1.count(num)){
+                answer[1].push_back(num);
+            }
+        }
+        return answer;
         }
 };
