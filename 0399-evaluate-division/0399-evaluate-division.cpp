7c5c8e5748078f917c7b031b7b42ec2bf681a355
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-10 21:55:59 +1100

    Time: 0 ms (100%), Space: 11.8 MB (56.46%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string, vector<pair<string, double>>> graph;
+        for (int i = 0; i < equations.size(); i++) {
+            string A = equations[i][0], B = equations[i][1];
+            double value = values[i];
+
+            graph[A].push_back({B, value});    // A / B = value
+            graph[B].push_back({A, 1.0 / value}); // B / A = 1 / value
+        }
+        vector<double> results;
+        for (auto& query : queries) {
+            unordered_map<string, bool> visited;
+            results.push_back(dfs(query[0], query[1], visited, graph));
+        }
+        return results;
+    }
+
+    double dfs(string u, string v, unordered_map<string,bool>& visited, unordered_map<string,vector<pair<string, double>>>& graph) {
+        if (graph.find(u) == graph.end() || graph.find(v) == graph.end()) {
+            return -1.0;
+        }
+        if (u == v) {
+            return 1.0;
+        }
+        visited[u] = true;
+
+        for (auto& [neighbor, weight] : graph[u]) {
+            if (!visited[neighbor]) {
+                double result = dfs(neighbor,v,visited,graph);
+                if (result != -1.0) return result * weight;
+            }
+        }
+
+        return -1.0;
+    }
+};
