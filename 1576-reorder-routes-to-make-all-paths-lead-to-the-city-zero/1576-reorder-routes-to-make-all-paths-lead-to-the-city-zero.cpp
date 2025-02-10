bbb7fb3af53c7d144f7caa3897f1365f69662fcc
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-10 20:44:43 +1100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    void dfs(int node, vector<vector<int>>& graph, set<pair<int,int>>& edges, vector<bool>& visit, int& changes){
+        visit[node] = true;
+        for (auto& neighbour : graph[node]){
+            if (!visit[neighbour]){
+                if (!edges.count({neighbour, node})) {
+                    changes++;
+                }
+                dfs(neighbour,graph,edges,visit,changes);
+            }
+        }
+    }
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> graph(n);
+        set<pair<int,int>> edges;
+
+        // Build adjacency list and store directed edges
+        for (auto& conn : connections) {
+            int u = conn[0], v = conn[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);  // Undirected representation
+            edges.emplace(u,v); // Mark original direction
+        }
+        int changes = 0;
+        vector<bool> visit(n);
+        dfs(0,graph,edges,visit,changes);
+        return changes; 
+    }
+};
