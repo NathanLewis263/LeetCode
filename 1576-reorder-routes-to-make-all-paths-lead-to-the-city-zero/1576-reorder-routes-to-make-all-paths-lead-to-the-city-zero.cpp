50e5d6a66ed103a8dceb2810da970830e561d3e1
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-10 20:51:38 +1100

    Time: 453 ms (8.61%), Space: 147.7 MB (17.06%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,27 +1,28 @@
 class Solution {
 private:
-    void dfs(int node, vector<vector<int>>& graph, set<pair<int,int>>& edges, vector<bool>& visit, int& changes){
+    void dfs(int node, vector<vector<int>>& graph, unordered_set<string>& edges, vector<bool>& visit, int& changes){
         visit[node] = true;
         for (auto& neighbour : graph[node]){
             if (!visit[neighbour]){
-                if (!edges.count({neighbour, node})) {
+                if (!edges.count(to_string(neighbour) + " " + to_string(node))) {
                     changes++;
                 }
                 dfs(neighbour,graph,edges,visit,changes);
             }
         }
     }
+
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<int>> graph(n);
-        set<pair<int,int>> edges;
+        unordered_set<string> edges;
 
         // Build adjacency list and store directed edges
         for (auto& conn : connections) {
             int u = conn[0], v = conn[1];
             graph[u].push_back(v);
             graph[v].push_back(u);  // Undirected representation
-            edges.emplace(u,v); // Mark original direction
+            edges.emplace(to_string(u) + " " + to_string(v)); // Mark original direction
         }
         int changes = 0;
         vector<bool> visit(n);
