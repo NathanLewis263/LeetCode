42feba155ac4b2c7e71b89df7cbd668152d3f5a1
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-10 22:45:35 +1100

    Time: 0 ms (100%), Space: 35.2 MB (77.85%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -1,45 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
         int rows = maze.size(), cols = maze[0].size();
-        int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}}; // Down, Up, Right, Left
+        int count = 0;
+        queue<pair<int,int>> q;
+        vector<vector<int>> direction = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        queue<pair<int, int>> q;
-        q.push({entrance[0], entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+'; // Mark visited
-        int steps = 0;
+        q.emplace(entrance[0], entrance[1]);
+        maze[entrance[0]][entrance[1]] = '+';
 
         while (!q.empty()) {
             int q_size = q.size();
-            steps++;
-
+            count++;
+            
             for (int i = 0; i < q_size; i++) {
                 auto [x, y] = q.front();
                 q.pop();
 
-                for (auto& dir : directions) {
-                    int nx = x + dir[0], ny = y + dir[1];
-
-                    // Check if the next position is valid
-                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && maze[nx][ny] == '.') {
-                        // If it's an exit and not the entrance, return the steps
+                for (auto& dir : direction) {
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];    
+                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && maze[nx][ny] == '.'){
                         if (nx == 0 || ny == 0 || nx == rows - 1 || ny == cols - 1) {
-                            return steps;
+                            return count;
                         }
-
-                        q.push({nx, ny});
-                        maze[nx][ny] = '+'; // Mark as visited
+                        q.emplace(nx,ny);
+                        maze[nx][ny] = '+';
                     }
                 }
             }
         }
-
-        return -1; // No exit found
+        return -1;    
     }
 };
