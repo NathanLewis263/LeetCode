d84ba590777173d2893b99372ebb9564092212f8
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-11 13:25:18 +1100

    Time: 0 ms (100%), Space: 16.9 MB (50.2%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> direction = {{1,0},{-1,0},{0,1},{0,-1}};
+        int fresh = 0;
+        for (int i = 0;i<rows;i++) {
+            for (int j = 0;j<cols;j++){
+                if (grid[i][j] == 2){
+                    q.emplace(i,j);
+                } else if (grid[i][j] == 1){
+                    fresh++;
+                }
+            }
+        }
+        if (fresh == 0){
+            return 0;
+        }
+
+        int min = 0;
+        while (!q.empty()){
+            int size = q.size();
+            bool rotted = false;
+            for (int i = 0;i<size;i++){
+                auto [x,y] = q.front();
+                q.pop();
+                for (auto& dir : direction){
+                    int nx = x + dir[0];
+                    int ny = y + dir[1];
+                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && grid[nx][ny] == 1){
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        fresh--;
+                        rotted = true;
+                    }
+                }
+            }
+            if (rotted){
+                min++;
+            }
+            if (fresh == 0){
+                return min;
+            }
+        }
+        return -1;
+    }
+};
