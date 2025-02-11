1d1c10bd9b59019a7e375c90e2ca4ea39a5d375b
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-12 00:07:51 +1100

    Time: 38 ms (70.5%), Space: 76.7 MB (90.64%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.cpp
@@ -12,12 +12,10 @@ public:
 
         while (k--){
             while (head.size() < candidates && left <= right){  //add candidates from the left (start)
-                head.push(costs[left]);
-                left++;
+                head.push(costs[left++]);
             }
             while (tail.size() < candidates && right >= left){  //add candidates from the right (end)
-                tail.push(costs[right]);
-                right--;
+                tail.push(costs[right--]);
             }
             if (tail.empty() || (!head.empty() && head.top() <= tail.top())){ 
                 cost += head.top();
