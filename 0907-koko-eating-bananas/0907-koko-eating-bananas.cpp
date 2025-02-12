44c2e48592e231b7e286e43875000aea4d11edbd
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-12 23:08:49 +1100

    Time: 9 ms (53.98%), Space: 22.8 MB (96.54%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -9,7 +9,7 @@ public:
             long long hours = 0;
             
             for (int pile : piles) {
-                hours += ceil(1.0*pile/mid);
+                hours += (pile + mid - 1) / mid;
             }
             
             if (hours <= h) {
