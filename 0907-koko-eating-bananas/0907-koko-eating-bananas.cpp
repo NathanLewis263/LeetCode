c3b12b8d89d8130ca10412bde78d4e01f652ba3d
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-12 23:07:37 +1100

    Time: 11 ms (44.5%), Space: 22.8 MB (96.54%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        sort(piles.begin(),piles.end());
-        int l = 1, r = piles[piles.size()-1];
-        
-        while (l < r) {
-            int mid = (l + r) / 2;
-            int hours = 0;
+        int l = 1;
+        int r = *max_element(piles.begin(),piles.end());
+        int result;
+        while (l <= r) {
+            int mid = l + (r-l) / 2;
+            long long hours = 0;
             
             for (int pile : piles) {
-                hours += ceil((double)pile/mid);
+                hours += (pile + mid - 1) / mid;
             }
             
             if (hours <= h) {
-                r = mid;
+                result = mid;
+                r = mid-1;
             } else {
                 l = mid + 1;
             }
         }
         
-        return l;
+        return result;
     }
 };
