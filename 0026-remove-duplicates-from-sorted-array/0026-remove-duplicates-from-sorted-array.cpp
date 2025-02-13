20f2cd348b9531a7940a62fb7099f06ca47f18f6
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-13 22:38:46 +1100

    Time: 6 ms (9.01%), Space: 23.6 MB (5.33%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,11 +4,9 @@ public:
         set<int> elements(nums.begin(),nums.end());
         int k = elements.size();
         auto it = elements.begin();
-        for (int i=0;i<nums.size();i++,it++){
+        for (int i=0;i<k;i++,it++){
             if (i < k){
                 nums.at(i) = *it;
-            } else {
-                nums.at(i) = 0;
             }
         }
         return k;
