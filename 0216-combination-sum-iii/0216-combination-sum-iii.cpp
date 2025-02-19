29e3984f12de40e717bfcf36233cd4335a0a31c9
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-19 12:11:30 +1100

    Time: 0 ms (100%), Space: 8.7 MB (60.87%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void backtrack(int start, int k, int n, vector<int>& current, vector<vector<int>>& result) {
+        // Base Case: If k elements are chosen and sum is n, add to result
+        if (current.size() == k && n == 0) {
+            result.push_back(current);
+            return;
+        }
+        
+        for (int i = start; i <= 9; i++) {
+            if (i > n) {
+                break; // if i is greater than n (sum) then break
+            }
+
+            current.push_back(i); // Choose number
+            backtrack(i + 1, k, n - i, current, result); // reduce n and explore other choices
+            current.pop_back(); // Undo choice
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(1, k, n, current, result);
+        return result;
+    }
+};
