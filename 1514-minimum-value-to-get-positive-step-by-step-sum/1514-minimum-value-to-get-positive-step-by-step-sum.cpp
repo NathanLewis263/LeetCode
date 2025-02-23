4eeed2e62c773c565e550adf8179451a833d27f2
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-24 10:56:28 +1100

    Time: 0 ms (100%), Space: 9.9 MB (78.49%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int minStartValue(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n);
-        prefix.at(0) = nums.at(0);
-        for (int i = 1;i<n;i++){
-            prefix.at(i) = prefix.at(i-1) + nums.at(i);
+        int total = 0;
+        int minVal = 0;
+        for (int i = 0;i<n;i++){
+            total += nums.at(i);
+            minVal = min(minVal, total);
         }
-        int minVal = *min_element(prefix.begin(),prefix.end());
-        return minVal < 0 ? abs(minVal)+1 : 1;
+        return abs(minVal)+1;
     }
 };
