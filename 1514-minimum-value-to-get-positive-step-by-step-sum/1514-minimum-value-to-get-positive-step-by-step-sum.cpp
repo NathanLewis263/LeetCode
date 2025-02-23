a645b24594d3c10309292b2fad3dfde28c562c3c
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-24 10:53:32 +1100

    Time: 0 ms (100%), Space: 10.5 MB (5.2%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -6,7 +6,6 @@ public:
         prefix.at(0) = nums.at(0);
         for (int i = 1;i<n;i++){
             prefix.at(i) = prefix.at(i-1) + nums.at(i);
-            cout << prefix.at(i) << " ";
         }
         int minVal = *min_element(prefix.begin(),prefix.end());
         return minVal < 0 ? abs(minVal)+1 : 1;
