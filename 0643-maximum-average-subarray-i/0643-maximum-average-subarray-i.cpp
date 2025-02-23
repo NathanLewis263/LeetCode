d8b21ea7f51b135b38e5309d832d65cd493a2d14
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-23 19:58:24 +1100

    Time: 8 ms (7.81%), Space: 113.6 MB (94.17%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         int n = nums.size();
-        int sum = 0;
+        double sum = 0;
         for (int i = 0;i<k;i++){
             sum+=nums.at(i);
         }
-        double maxAvg = sum/(double)k;
+        double maxAvg = sum;
         for (int i = k;i<n;i++){
             sum+=nums.at(i);
             sum-=nums.at(i-k);
-            maxAvg = max(maxAvg,sum/(double)k);
+            maxAvg = max(maxAvg,sum);
         }
-        return maxAvg;
+        return maxAvg/k;
     }
 };
