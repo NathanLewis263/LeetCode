ab53b68ac5fce954954ad12e3d4993fb8fac8551
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-24 12:26:24 +1100

    Time: 3 ms (54.47%), Space: 12.1 MB (90.91%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,13 +3,16 @@ public:
     string reverseWords(string s) {
         int n = s.size();
         int left = 0;
-        for (int i = 0;i<=n;i++){
-            if (i == n || s[i] == ' '){
-                for (int l = left, r = i - 1; l < r; l++, r--) { 
-                    swap(s[l], s[r]);
+        int right = 0;
+        while (right <= n){
+            if (right == n || s[right] == ' '){
+                int l = left, r = right-1;
+                while (l<r){
+                    swap(s[l++], s[r--]);
                 }
-                left = i + 1; // Move to the next word
+                left = right + 1; 
             }
+            right++;
         }
         return s;
     };
