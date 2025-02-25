1b2939cd04d0f7eb8f43e424892badaa06145984
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-02-25 18:08:40 +1100

    Time: 115 ms (88.32%), Space: 165.3 MB (98.34%) - LeetHub

--- 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/1354-find-players-with-zero-or-one-losses.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int,int> map;
+        unordered_map<int,int> lossCount;
         for (auto& match : matches){
-            map[match[0]];
-            map[match[1]]++;
+            lossCount[match[0]]; // Ensure winners exist in the map
+            lossCount[match[1]]++; // Increment loss count for losers
         }
         vector<vector<int>> ans(2);
-        for (auto& pair : map){
+        for (auto& pair : lossCount){
             if (pair.second == 0){
-                ans[0].push_back(pair.first);
+                ans[0].push_back(pair.first); // Players with 0 losses
             } else if (pair.second == 1){
-                ans[1].push_back(pair.first);
+                ans[1].push_back(pair.first); // Players with 1 loss
             }
         }
+        
+        // Sort for increasing order
         sort(ans[0].begin(),ans[0].end());
         sort(ans[1].begin(),ans[1].end());
         return ans;
