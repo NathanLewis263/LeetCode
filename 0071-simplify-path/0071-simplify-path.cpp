c9bef1d4257ff22090cd196b81bf114ce80ea499
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-03-07 21:51:25 +1100

    Time: 1 ms (73.9%), Space: 10.5 MB (84.53%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> stack;
+        stringstream ss(path);
+        string temp;
+        while (getline(ss, temp, '/')) {
+            if (temp == "..") {
+                if (!stack.empty()) {
+                    stack.pop_back();
+                }
+            } else if (temp != "." && !temp.empty()) {
+                stack.push_back(temp);
+            }
+        }
+        string res = "";
+        for (string& s : stack){
+            res += "/" + s;
+        }
+        if (res.empty()){
+            return "/";
+        } else {
+            return res;
+        }
+    }
+};
