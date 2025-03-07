b6dd4fba6c25faa5ad8dd44a47e257cb507aa6fe
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-03-07 22:01:14 +1100

    Time: 0 ms (100%), Space: 8.4 MB (67.6%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string stack = "";
+        for (char& c: s){
+            if (!stack.empty() && abs(stack.back() - c) == 32){
+                stack.pop_back();
+            }
+            else {
+                stack.push_back(c);
+            }
+        }
+        return stack;
+    }
+};
