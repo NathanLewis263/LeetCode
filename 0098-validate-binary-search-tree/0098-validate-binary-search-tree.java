8265603b47acc471d8c61626d5f312f6e5b5bd2a
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2025-05-01 00:27:08 +1000

    Time: 0 ms (100%), Space: 44.5 MB (17.4%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Integer prev = null;
+
+    public boolean isValidBST(TreeNode root) {
+        return inorder(root);
+    }
+
+    private boolean inorder(TreeNode node) {
+        if (node == null) return true;
+        // 1) visit left
+        if (!inorder(node.left)) return false;
+        // 2) check current
+        if (prev != null && node.val <= prev) return false;
+        prev = node.val;
+        // 3) visit right
+        return inorder(node.right);
+    }
+}
