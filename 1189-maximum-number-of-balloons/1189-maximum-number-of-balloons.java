d3dad9e5f0fdeba280d930026c7ccf5419efaf07
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2026-01-18 10:20:11 +0530

    Time: 3 ms (69.35%), Space: 43.6 MB (47.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,15 +1,25 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character, Integer> count = new HashMap<>();
+        int[] counts = new int[5];
         for (char letter : text.toCharArray()){
-            count.put(letter, count.getOrDefault(letter,0)+1);
+            if (letter == 'b'){
+                counts[0]++;
+            } else if (letter == 'a') {
+                counts[1]++;
+            } else if (letter == 'l') {
+                counts[2]++;
+            } else if (letter == 'o') {
+                counts[3]++; 
+            } else if (letter == 'n') {
+                counts[4]++;
+            }
         }
-        int res = count.getOrDefault('b', 0);
-        res = Math.min(res, count.getOrDefault('a', 0));
-        res = Math.min(res, count.getOrDefault('l', 0) / 2); // Need 2 'l's
-        res = Math.min(res, count.getOrDefault('o', 0) / 2); // Need 2 'o's
-        res = Math.min(res, count.getOrDefault('n', 0));
-        return res;
+        int ans = counts[0];
+        ans = Math.min(ans, counts[1]);
+        ans = Math.min(ans, counts[2]/2);
+        ans = Math.min(ans, counts[3]/2);
+        ans = Math.min(ans, counts[4]);
+        return ans;
 
     }
 }
