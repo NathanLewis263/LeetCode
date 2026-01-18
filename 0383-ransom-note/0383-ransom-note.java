9230ac7e69760c94b49f1b12d70a93390164f054
Nathan lewis <144401140+NathanLewis263@users.noreply.github.com>
2026-01-18 14:47:51 +0530

    Time: 15 ms (36.69%), Space: 47.1 MB (32.69%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for (char letter : magazine.toCharArray()){
+            freq.put(letter, freq.getOrDefault(letter,0)+1);
+        }
+        for (char letter : ransomNote.toCharArray()){
+            if (freq.containsKey(letter)){
+                freq.put(letter, freq.getOrDefault(letter,0)-1);
+                if (freq.get(letter) == 0){
+                    freq.remove(letter);
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
